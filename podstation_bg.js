bcde0afbe58c087cff3a789504950da5fc005072
Guilherme Dellagustin <dellagustin@gmail.com>
2015-07-19 18:02:13 +0200

    Initial version
    This version is just an initial mockup.
    It contains a button that opens the main page.
    The main page can only be opened once, pressing the button
    again will focus the already opened one.
    It also contains an empty options page.

---
+++ podstation_bg.js
@@ -0,0 +1,11 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.query({url: 'chrome-extension://*/podstation.html'}, function(tabs) {
+		if(tabs.length) {
+			chrome.tabs.update(tabs[0].id, {active: true});
+			chrome.windows.update(tabs[0].windowId, {focused: true});
+		}
+		else {
+			window.open('/podstation.html');
+		}
+	});
+});
