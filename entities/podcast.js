f3cccdea57008184900bfeba34eb40ceeb5893b4
Guilherme Dellagustin <dellagustin@gmail.com>
2015-08-20 23:22:08 +0200

    Added podcast storage and status
    Podcast object now stores data locally and has a status (new, failed,
    loaded, etc...)

--- entities/podcast.js
+++ entities/podcast.js
@@ -1,9 +1,31 @@
 var Podcast = function(url) {
 	this.url = url;
 	this.image = 'images/rss_small.png';
+	this.status = 'new';
+
+	this.getKey = function() {
+		return 'podcast' + this.url;
+	}
+
+	this.store = function() {
+		var storageObject = {};
+
+		var storedPodcast = {};
+
+		storedPodcast.title = this.title;
+		storedPodcast.description = this.description;
+		storedPodcast.link = this.link;
+		storedPodcast.image = this.image;
+
+		storageObject[this.getKey()] = storedPodcast;
+
+		chrome.storage.local.set(storageObject);
+	}
 
 	this.update = function(callback) {
 		var that = this;
+
+		this.status = 'updating';
 
 		$.get(this.url, function(data) {
 			var xml = $(data);
@@ -12,22 +34,30 @@ var Podcast = function(url) {
 			that.description = xml.find('rss > channel > description').text();
 			that.link = xml.find('rss > channel > link').text();
 			that.image = xml.find('rss > channel > image > url').text();
+
 			if(that.image === "") {
 				that.image = xml.find('rss > channel > image').attr('href');
 			}
 
+			that.status = 'loaded';
+
+			that.store();
+
 			if(typeof callback === "function") {
 				callback(that);
 			}
-		});
+
+		}/*, function() {
+			that.status = 'failed';
+		}*/);
 	}
 
 	this.load = function(callback) {
 		var that = this;
 
-		var podcastKey = 'podcast' + this.url;
+		var podcastKey = this.getKey();
 
-		chrome.storage.sync.get(podcastKey, function(storageObject) {
+		chrome.storage.local.get(podcastKey, function(storageObject) {
 			if(storageObject && storageObject[podcastKey]) {
 				var storedPodcast = storageObject[podcastKey];
 
