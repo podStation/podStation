d58364af4048d60d6853bd848ebaf0c82cc613a5
Guilherme Dellagustin <dellagustin@gmail.com>
2015-08-23 10:15:50 +0200

    Added episode viewing

---
+++ ui/episodeHTMLRenderer.js
@@ -0,0 +1,27 @@
+var episodeTemplate;
+
+jQuery.ajax({
+	url: chrome.extension.getURL('templates/episode.mustache'),
+	dataType: "text",
+	success: function(result) {
+		episodeTemplate = result;
+	},
+	async: false
+});
+
+function renderEpisode(episode) {
+	var episodeHTML;
+	var episodeForTemplate;
+
+	episodeForTemplate = {
+		link: episode.link,
+		title: episode.title ? episode.title : episode.url,
+		url: episode.enclosure.url,
+		description: episode.description,
+		pubDate: episode.pubDate
+	};
+
+	var renderedHTML = Mustache.render(episodeTemplate, episodeForTemplate);
+
+	return renderedHTML;
+}
