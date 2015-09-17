ab78632b145ae13127351fbb648d91f71f472a1c
Guilherme Dellagustin <dellagustin@gmail.com>
2015-09-17 19:35:19 -0300

    Show feeds that are already added in feedfinder
    Now when you open the Feed Finder popup (in pages that contain feeds)
    the feeds you have already added are show with a check.

--- popup.js
+++ popup.js
@@ -4,25 +4,26 @@ $(document).ready(function() {
 		var tab = tabs[0];
 
 		chrome.tabs.sendMessage(tab.id, {action: 'getFeeds'}, function(response) {
-
 			if(response.length) {
-				var html = '';
+				chrome.runtime.getBackgroundPage(function(bgPage) {
+					var html = '';
 
-				html = renderFeedsInPage(response);
-
-				$('#feedsInPage').html(html);
-
-				$('#open').click(function(event) {
-					event.preventDefault();
-					chrome.runtime.getBackgroundPage(function(bgPage) {
-						bgPage.openPodStation();
+					response.forEach(function(item) {
+						item.added = bgPage.podcastManager.getPodcast(item.feed) !== undefined;
 					});
-				});
 
-				$('.addPodcast').click(function(event) {
-					event.preventDefault();
+					html = renderFeedsInPage(response);
 
-					chrome.runtime.getBackgroundPage(function(bgPage) {
+					$('#feedsInPage').html(html);
+
+					$('#open').click(function(event) {
+						event.preventDefault();
+						bgPage.openPodStation();
+						;
+					});
+
+					$('.addPodcast').click(function(event) {
+						event.preventDefault();
 						bgPage.podcastManager.addPodcast(event.currentTarget.id)
 						bgPage.openPodStation('Podcasts');
 					});
