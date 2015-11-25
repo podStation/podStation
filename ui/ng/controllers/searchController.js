0256bdea706c732940499f9559cee038f2e78cba
Guilherme Dellagustin <dellagustin@gmail.com>
2015-11-25 23:35:25 +0100

    Added search results from iTunes
    Search now also in iTunes.
    Merges results with the same feed url.

--- ui/ng/controllers/searchController.js
+++ ui/ng/controllers/searchController.js
@@ -1,43 +1,116 @@
 myApp.controller('searchController', ['$scope', '$routeParams', '$http', '$location', function($scope, $routeParams, $http, $location) {
 	$scope.searchTerms = $routeParams.searchTerms;
 
-	var url = 'http://api.digitalpodcast.com/v2r/search/?appid=0f56b00cfbdc051c29b88171e67507f3&format=rssopml&keywords=' + $scope.searchTerms;
+	$scope.searchResults = [];
 
-	$http.get(url).then(function(response) {
-		$scope.searchResults = [];
+	var SearchResult = function() {
+		this.title = '';
+		this.feedUrl = '';
+		this.description = '';
+		this.link = '';
+		this.itunesLink = '';
+		this.image = '';
 
-		var xml = $($.parseXML(response.data));
-
-		xml.find('opml > body > outline').each(function() {
-			var feed = $(this);
-
-			var searchResult = {};
-
-			searchResult.title = feed.attr('text');
-			searchResult.description = feed.attr('description');
-			searchResult.link = feed.attr('htmlUrl');
-			searchResult.feedUrl = feed.attr('xmlUrl');
-			searchResult.subscribed = false;
+		this.addPodcast = function() {
+			var that = this;
 
 			chrome.runtime.getBackgroundPage(function(bgPage) {
 				$scope.$apply(function() {
-					searchResult.subscribed = bgPage.podcastManager.getPodcast(searchResult.feedUrl) !== undefined;
+					bgPage.podcastManager.addPodcast(that.feedUrl);
+
+					$location.path('/Podcasts');
 				});
 			});
+		};
 
-			searchResult.addPodcast = function() {
-				var that = this;
+		this.checkIsSubscribed = function() {
+			var that = this;
 
-				chrome.runtime.getBackgroundPage(function(bgPage) {
-					$scope.$apply(function() {
-						bgPage.podcastManager.addPodcast(that.feedUrl);
-
-						$location.path('/Podcasts');
-					});
+			chrome.runtime.getBackgroundPage(function(bgPage) {
+				$scope.$apply(function() {
+					that.subscribed = bgPage.podcastManager.getPodcast(that.feedUrl) !== undefined;
 				});
-			};
+			});
+		};
 
-			$scope.searchResults.push(searchResult);
+		this.merge = function(otherSearchResult) {
+			if(otherSearchResult.description !== '')
+				this.description = otherSearchResult.description;
+
+			if(otherSearchResult.link !== '')
+				this.link = otherSearchResult.link;
+
+			if(otherSearchResult.image !== '')
+				this.image = otherSearchResult.image;
+
+			if(otherSearchResult.itunesLink !== '')
+				this.itunesLink = otherSearchResult.itunesLink;
+		}
+
+		return this;
+	}
+
+	function mergeInResult(searchResult) {
+		var searchResultToMergeWith = $scope.searchResults.find(function(item) {
+			return item.feedUrl === searchResult.feedUrl;
 		});
-	});
+
+		if(searchResultToMergeWith) {
+			searchResultToMergeWith.merge(searchResult);
+		}
+		else {
+			searchResult.checkIsSubscribed();
+			$scope.searchResults.push(searchResult);
+		}
+	}
+
+	function searchInDigitalPodcast() {
+		var url = 'http://api.digitalpodcast.com/v2r/search/?appid=0f56b00cfbdc051c29b88171e67507f3&format=rssopml&keywords=' + $scope.searchTerms;
+
+		$http.get(url).then(function(response) {
+
+			var xml = $($.parseXML(response.data));
+
+			xml.find('opml > body > outline').each(function() {
+				var feed = $(this);
+
+				var searchResult = new SearchResult;
+
+				searchResult.title = feed.attr('text');
+				searchResult.description = feed.attr('description');
+				searchResult.link = feed.attr('htmlUrl');
+				searchResult.feedUrl = feed.attr('xmlUrl');
+				searchResult.subscribed = false;
+
+				mergeInResult(searchResult);
+			});
+		});
+	}
+
+	function searchItunes() {
+		var url = 'https://itunes.apple.com/search?media=podcast&term=' + $scope.searchTerms;
+
+		$http.get(url).then(function(response) {
+			response.data.results.forEach(function(result) {
+				var searchResult = new SearchResult;
+
+				searchResult.title = result.collectionName;
+				searchResult.itunesLink = result.collectionViewUrl;
+				searchResult.feedUrl = result.feedUrl;
+				searchResult.subscribed = false;
+				searchResult.image = result.artworkUrl100;
+
+				mergeInResult(searchResult);
+			});
+		});
+	}
+
+	function search() {
+		$scope.searchResults = [];
+
+		searchItunes();
+		searchInDigitalPodcast();
+	}
+
+	search();
 }]);
