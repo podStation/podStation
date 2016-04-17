1a819ef7955d5009d0f4d2fd81a7a4bdf31e99c8
Guilherme Dellagustin <dellagustin@gmail.com>
2016-04-17 15:35:47 +0200

    Remove unused comments

--- ui/ng/controllers/optionsController.js
+++ ui/ng/controllers/optionsController.js
@@ -5,25 +5,10 @@ myApp.controller('optionsController', ['$scope', 'messageService', function($sco
 	$scope.options.autoUpdateEvery = 60;
 
 	$scope.save = function() {
-		/*chrome.runtime.sendMessage({
-			to: 'optionsManager',
-			type: 'saveOptions',
-			options: $scope.options
-		});*/
-		
 		messageService.for('optionsManager').sendMessage('saveOptions', $scope.options);
 	};
 
 	function readOptions() {
-		/*chrome.runtime.sendMessage({
-			to: 'optionsManager',
-			type: 'getOptions',
-		}, function(options) {
-			$scope.$apply(function() {
-				$scope.options = options;
-			});
-		});*/
-		
 		messageService.for('optionsManager').sendMessage('getOptions', {}, function(options) {
 			$scope.$apply(function() {
 				$scope.options = options;
