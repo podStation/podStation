85c69cadfa7415ebeb67b78170f295cee3774f99
Guilherme Dellagustin <dellagutin@gmail.com>
2017-03-01 23:11:46 +0100

    [chromecast] added button to menu
    Just experimenting, I have the feeling that this scripts and
    button will have to be handled in the background page at the
    end.

--- podstation.js
+++ podstation.js
@@ -7,3 +7,16 @@ $(document).ready(function() {
 		});
 	});
 });
+
+window['__onGCastApiAvailable'] = function(isAvailable) {
+	if (isAvailable) {
+		initializeCastApi();
+	}
+};
+
+initializeCastApi = function() {
+	cast.framework.CastContext.getInstance().setOptions({
+		receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
+		autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+	});
+};
