260d1362a04cdbf209a961421b8226b5f3fd7395
Guilherme Dellagustin <dellagutin@gmail.com>
2017-09-18 22:49:42 +0200

    Removed dupplicated angular run

--- background/entities/backgroundApp.js
+++ background/entities/backgroundApp.js
@@ -4,10 +4,6 @@ angular.module('podstationBackgroundApp').factory('browser', function() {
 	return chrome;
 });
 
-angular.module('podstationBackgroundApp').run(['playlist', 'analyticsService', function(playlist, analyticsService) {
-	// this is only to ensure the services are created as soon as possible
-}]);
-
 angular.module('podstationBackgroundApp').run(['$window', 'playlist', 'browser', 'analyticsService', 
 function($window, playlist, browser, analyticsService) {
 	
