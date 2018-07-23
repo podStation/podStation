b7f0e28fe1428fdfde4fc27e236979cfeec44d72
Guilherme <dellagustin@gmail.com>
2018-07-23 23:10:59 +0200

    [deploy] Create zip file for deployment

--- check_should_deploy_and_create_new_tag.sh
+++ check_should_deploy_and_create_new_tag.sh
@@ -7,5 +7,8 @@ if [ $last_version_tag != $current_version ]
 then
     echo "Version changed to" $current_version ", previous was" $last_version_tag
     export NEW_VERSION=$current_version
+    echo "Creating new git tag"
     git tag $NEW_VERSION
+    echo "Creating zip file for release"
+    ./create_zip_for_upload.sh
 fi
