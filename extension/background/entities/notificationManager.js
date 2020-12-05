6b7c3aebb95d21c38a75f47b3aa9f4bb109a59cb
Guilherme Dellagustin <dellagustin@gmail.com>
2020-12-05 16:53:06 +0100

    Updated version to 1.31.0

--- extension/background/entities/notificationManager.js
+++ extension/background/entities/notificationManager.js
@@ -12,7 +12,8 @@
 		'1.26.0': {url: 'https://github.com/podStation/podStation/tree/v1.26.0/docs/release_notes/v1.26.0.md'},
 		'1.27.0': {url: 'https://github.com/podStation/podStation/tree/v1.27.0/docs/release_notes/v1.27.0.md'},
 		'1.28.0': {url: 'https://github.com/podStation/podStation/tree/v1.28.0/docs/release_notes/v1.28.0.md'},
-		'1.29.0': {url: 'https://github.com/podStation/podStation/tree/v1.29.0/docs/release_notes/v1.29.0.md'}
+		'1.29.0': {url: 'https://github.com/podStation/podStation/tree/v1.29.0/docs/release_notes/v1.29.0.md'},
+		'1.31.0': {url: 'https://github.com/podStation/podStation/tree/v1.31.0/docs/release_notes/v1.31.0.md'}
 	});
 
 	angular.module('podstationBackgroundApp')
