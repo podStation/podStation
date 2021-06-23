7ed52db8b719de395689ca9bdae359046309b1d2
Guilherme Dellagustin <dellagustin@gmail.com>
2021-06-23 23:15:43 +0200

    Changed karma.conf.js identation to tags

--- karma.conf.js
+++ karma.conf.js
@@ -6,102 +6,102 @@ const jasmineSeedReporter = require('./spec/support/jasmine-seed-reporter.js')
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+	// base path that will be used to resolve all patterns (eg. files, exclude)
+	basePath: '',
 
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+	// frameworks to use
+	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+	frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'extension/lib/jquery.min.js',
-      'extension/lib/angular.js',
-      'extension/lib/jsmediatags.min.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'extension/reuse/**/*.js',
-      'extension/background/entities/backgroundApp.js',
-      'extension/background/entities/audioPlayer.js',
-      'extension/background/entities/episodeSelector.js',
-      'extension/background/entities/podcastManager.js',
-      'extension/background/entities/notificationManager.js',
-      'extension/background/entities/optionsManager.js',
-      'extension/background/entities/playlist.js',
-      'extension/background/entities/podcast.js',
-      'extension/background/ng/**/*.js',
-      'spec/background/**/*.js',
-      'spec/reuse/**/*.js',
-      {pattern: 'spec/background/**/*.xml', included: false},
-      {pattern: 'spec/background/**/*.opml', included: false},
-      {pattern: 'spec/background/**/*.json', included: false}
-    ],
-
-
-    // list of files / patterns to exclude
-    exclude: [
-    ],
+	// list of files / patterns to load in the browser
+	files: [
+		'extension/lib/jquery.min.js',
+		'extension/lib/angular.js',
+		'extension/lib/jsmediatags.min.js',
+		'bower_components/angular-mocks/angular-mocks.js',
+		'extension/reuse/**/*.js',
+		'extension/background/entities/backgroundApp.js',
+		'extension/background/entities/audioPlayer.js',
+		'extension/background/entities/episodeSelector.js',
+		'extension/background/entities/podcastManager.js',
+		'extension/background/entities/notificationManager.js',
+		'extension/background/entities/optionsManager.js',
+		'extension/background/entities/playlist.js',
+		'extension/background/entities/podcast.js',
+		'extension/background/ng/**/*.js',
+		'spec/background/**/*.js',
+		'spec/reuse/**/*.js',
+		{pattern: 'spec/background/**/*.xml', included: false},
+		{pattern: 'spec/background/**/*.opml', included: false},
+		{pattern: 'spec/background/**/*.json', included: false}
+	],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-    plugins: [
-      'karma-*',
-      jasmineSeedReporter
-    ],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'jasmine-seed'],
-
-    client: {
-      jasmine: {
-        random: true,
-        // seed: 19141 // Specify if you need to re-run the same seed
-      }
-    },
-
-    // web server port
-    port: 9876,
+	// list of files / patterns to exclude
+	exclude: [
+	],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+	// preprocess matching files before serving them to the browser
+	// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	preprocessors: {
+	},
+
+	plugins: [
+		'karma-*',
+		jasmineSeedReporter
+	],
+
+	// test results reporter to use
+	// possible values: 'dots', 'progress'
+	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+	reporters: ['progress', 'jasmine-seed'],
+
+	client: {
+		jasmine: {
+			random: true,
+			// seed: 19141 // Specify if you need to re-run the same seed
+		}
+	},
+
+	// web server port
+	port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+	// enable / disable colors in the output (reporters and logs)
+	colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+	// level of logging
+	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	logLevel: config.LOG_INFO,
 
-    customLaunchers: {
-      ChromeDebugging: {
-        base: 'Chrome',
-        flags: [ '--remote-debugging-port=9333' ]
-      },
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'ChromeDebugging', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+	// enable / disable watching file and executing tests whenever any file changes
+	autoWatch: true,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+	customLaunchers: {
+		ChromeDebugging: {
+			base: 'Chrome',
+			flags: [ '--remote-debugging-port=9333' ]
+		},
+		ChromeHeadlessNoSandbox: {
+			base: 'ChromeHeadless',
+			flags: ['--no-sandbox']
+		}
+	},
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+	// start these browsers
+	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	browsers: ['Chrome', 'ChromeDebugging', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+
+	// Continuous Integration mode
+	// if true, Karma captures browsers, runs the tests and exits
+	singleRun: false,
+
+	// Concurrency level
+	// how many browser should be started simultaneous
+	concurrency: Infinity
   })
 }
