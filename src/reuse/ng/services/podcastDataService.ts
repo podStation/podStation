4ef4a958e179cec9b5bbc6365ac4ce331ad3c555
Guilherme Dellagustin <dellagustin@gmail.com>
2022-03-19 18:01:37 +0100

    Converted PodcastDataService to ts

--- src/reuse/ng/services/podcastDataService.ts
+++ src/reuse/ng/services/podcastDataService.ts
@@ -1,31 +1,86 @@
+
+/*
+Problems:
+- determinePodcastUrl relies on the podcast property `url`, while podcastId relies on `feedUrl`, this is an inconsistency
+- episodeId uses the episode property `link` while episodeMatchesId uses `url`, this was probably not intended
+*/
+
 /**
  * A non-persistent identifier for a podcast
- * @typedef {Object} PodcastId
  */
+type PodcastId = {
+	values: {
+		/**
+		 * Feed url of a podcast
+		 */
+		url: string
+	}
+}
+
+type PodcastForIdDetermination = {
+	feedUrl: string;
+}
 
 /**
  * A non-persistent identifier for an episode
- * @typedef {Object} EpisodeId
+ * guid, title and link are optional, but at least one of them
+ * should have a value.
  */
+ type EpisodeId = {
+	values: {
+		/**
+		 * Feed url of a podcast
+		 */
+		podcastUrl: string,
+		guid?: string,
+		title?: string,
+		link?: string,
+		url?: string,
+	}
+}
 
-'use strict';
+type EpisodeForIdDetermination = {
+	podcastUrl?: string,
+	guid?: string,
+	link?: string,
+	title?: string,
+	url?: string,
+}
 
-function podcastDataService() {
-	var service = {
-		podcastId: podcastId,
-		episodeId: episodeId,
-		episodeMatchesId: episodeMatchesId,
-		episodeIdEqualsId: episodeIdEqualsId
-	};
+ function determinePodcastUrl(episode: EpisodeForIdDetermination, podcast: any): string {
+	var url;
+	if(episode.podcastUrl) {
+		url = episode.podcastUrl;
+	}
+	else if (typeof podcast === 'string') {
+		url = podcast;
+	}
+	else if (podcast) {
+		url = podcast.url;
+	}
 
-	return service;
+	if(!url) {
+		throw Error('could not determine podcastUrl for podcastId');
+	}
 
+	return url;
+}
+
+function PodcastDataService() {
+	return new PodcastDataServiceClass();
+}
+
+// This does not have to be a class a all, the functions could exist individually.
+// It was created as an angular service before we introduced webpack and the usage of modules.
+// I cannot remember exactly why it was an angular service, maybe to avoid adding too many global
+// functions (again, before we used modules).
+// I'll keep it as a class for now to keep it backwards compatible, but this could be
+// refactored.
+class PodcastDataServiceClass {
 	/**
 	 * Returns and non-persistent identifier for a podcast
-	 * @param {Podcast} podcast 
-	 * @return {PodcastId}
 	 */
-	function podcastId(podcast) {
+	podcastId(podcast: PodcastForIdDetermination): PodcastId {
 		return {
 			values: {
 				url: podcast.feedUrl
@@ -35,12 +90,10 @@ function podcastDataService() {
 
 	/**
 	 * Returns an in-memory identifier for a podcast episode
-	 * @param {Episode} episode
 	 * @param {Podcast} podcast optional if episode contains `podcastUrl`
-	 * @return {EpisodeId}
 	 */
-	function episodeId(episode, podcast) {
-		const _episodeId = {
+	episodeId(episode: EpisodeForIdDetermination, podcast?: any) : EpisodeId {
+		const _episodeId: EpisodeId = {
 			values: {
 				podcastUrl: determinePodcastUrl(episode, podcast),
 			}
@@ -57,7 +110,7 @@ function podcastDataService() {
 	 * Checks if a given episode and podcast matches an episodeId
 	 * @param {EpisodeId} episodeId
 	 */
-	function episodeMatchesId(episode, podcast, episodeId) {
+	episodeMatchesId(episode: EpisodeForIdDetermination, podcast: any, episodeId: EpisodeId): boolean {
 		var podcastUrl = determinePodcastUrl(episode, podcast);
 
 		// if no podcast url can be determined, ignore it
@@ -88,14 +141,18 @@ function podcastDataService() {
 	 * @param {EpisodeId} episodeId2 
 	 * @returns {Boolean}
 	 */
-	function episodeIdEqualsId(episodeId1, episodeId2) {
+	episodeIdEqualsId(episodeId1: EpisodeId, episodeId2: EpisodeId): boolean {
 		if(episodeId1 === episodeId2)
 			return true;
 
 		if(!(episodeId1 && episodeId2))
 			return false;
 
-		for(var key in episodeId1.values) {
+		/**
+		 * TODO: possible bug here, if episodeId2 contains more properties than episodeId1, this check would not lead to a false result
+		 */
+		let key: keyof EpisodeId["values"];
+		for(key in episodeId1.values) {
 			if(episodeId1.values[key] !== episodeId2.values[key]){
 				return false;
 			}
@@ -103,25 +160,6 @@ function podcastDataService() {
 
 		return true;
 	}
-
-	function determinePodcastUrl(episode, podcast) {
-		var url;
-		if(episode.podcastUrl) {
-			url = episode.podcastUrl;
-		}
-		else if (typeof podcast === 'string') {
-			url = podcast;
-		}
-		else if (podcast) {
-			url = podcast.url;
-		}
-
-		if(!url) {
-			throw Error('could not determine podcastUrl for podcastId');
-		}
-
-		return url;
-	}
 }
 
-export default podcastDataService;
+export default PodcastDataService;
