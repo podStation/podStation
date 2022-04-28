e2b3edacc57f35ff8c4ef1f600805df04e5717b4
Guilherme Dellagustin <dellagustin@gmail.com>
2022-04-28 22:54:40 +0200

    Refactoring: MessageService converted to typescript

--- src/reuse/messageServiceDefinition.ts
+++ src/reuse/messageServiceDefinition.ts
@@ -1,96 +1,21 @@
-import angular from 'angular';
+type SendResponseCallback = (response: any) => void;
+type OnMessageCallback = (messageContent: any, sendResponseCallback: SendResponseCallback) => boolean;
 
-var instance;
+interface IMessageRelayer {
+	sendMessage(messageName: string, messageContent: any, sendResponseCallback: any): void;
+	onMessage(messageName: string, callback: any): IMessageRelayer;
+}
 
-function ChromeExtensionMessageService(isBackgroundPage) {
-	if(instance) {
-		return instance;
-	}
+class ChromeExtensionMessageService {
 
-	instance = this;
+	private runningInBackground: boolean;
+	private messageRelayers: Record<string, MessageRelayer> = {};
+	private browserService: any;
 
-	var runningInBackground = isBackgroundPage;
-	var messageRelayers = {};
-
-	function MessageRelayer(name) {
-		this.name = name;
-		this.listeners = {};
-
-		var that = this;
-
-		this.onMessage = function(messageName, callback) {
-			if(!this.listeners[messageName]) {
-				this.listeners[messageName] = [];
-			}
-			else if(this.listeners[messageName].indexOf(callback) >= 0) {
-				return;
-			}
-
-			this.listeners[messageName].push(callback);
-			return this;
-		}
-
-		function dispatchToListeners(messageName, messageContent, sendResponseCallback, responseTakenCareOf) {
-			if(!that.listeners[messageName]) {
-				return false;
-			}
-			
-			var willAnswer;
-
-			that.listeners[messageName].forEach(function(callback) {
-				willAnswer = callback(messageContent, function(response) {
-					if(!responseTakenCareOf && sendResponseCallback) {
-						responseTakenCareOf = true;
-						sendResponseCallback(response);
-					}
-				}) ? true : willAnswer;
-			});
-
-			return willAnswer;
-		}
-
-		this.sendMessage = function(messageName, messageContent, sendResponseCallback) {
-			var responseTakenCareOf = false;
-
-			// send the message to other receivers than the background
-			getBrowserService().runtime.sendMessage({
-				fromBackground: runningInBackground,
-				relayerName: this.name,
-				name: messageName,
-				content: messageContent,
-			}, function(response) {
-				// check if it was already answered by a background listener
-				if(!responseTakenCareOf && sendResponseCallback) {
-					responseTakenCareOf = true;
-					sendResponseCallback(response);
-				}
-			});
-
-			if(runningInBackground) {
-				// messages sent from foreground are always treated on the runtime listener
-				dispatchToListeners(messageName, messageContent, sendResponseCallback, responseTakenCareOf);
-			}
-		}
-
-		getBrowserService().runtime.onMessage.addListener(function(message, sender, sendResponse) {
-			if(runningInBackground && message.fromBackground) {
-				// already taken care of internally
-				return;
-			}
-			
-			if(message.relayerName != that.name) {
-				// not for me
-				return;
-			}
-
-			var responseTakenCareOf = false;
-
-			return dispatchToListeners(message.name, message.content, sendResponse, responseTakenCareOf);
-		});
-
-		function getBrowserService() {
-			return angular.element(document.body).injector() ? angular.element(document.body).injector().get('browser') : chrome;
-		}
+	constructor(isBackgroundPage: boolean, browserService: any) {
+		this.runningInBackground = isBackgroundPage;
+		this.browserService = browserService;
+		this.registerMessageListener();
 	}
 
 	/**
@@ -98,16 +23,109 @@ function ChromeExtensionMessageService(isBackgroundPage) {
 	 * @param {string} relayerName 
 	 * @returns {MessageRelayer}
 	 */
-	this.for = function(relayerName) {
-		if(!messageRelayers[relayerName]) {
-			messageRelayers[relayerName] = new MessageRelayer(relayerName);
+	for(relayerName: string): IMessageRelayer {
+		if(!this.messageRelayers[relayerName]) {
+			this.messageRelayers[relayerName] = new MessageRelayer(relayerName, this.runningInBackground, this.browserService);
 		}
 		
-		return messageRelayers[relayerName];
+		return this.messageRelayers[relayerName];
 	}
 
-	this.reset = function() {
-		messageRelayers = {};
+	reset() {
+		this.messageRelayers = {};
+	}
+
+	private registerMessageListener() {
+		this.browserService.runtime.onMessage.addListener((message: any, sender: any, sendResponse: SendResponseCallback) => {
+			if(this.runningInBackground && message.fromBackground) {
+				// already taken care of internally
+				return;
+			}
+
+			if(message.relayerName) {
+				const relayer = this.messageRelayers[message.relayerName];
+
+				if(relayer) {
+					let responseTakenCareOf = false;
+		
+					return relayer.dispatchToListeners(message.name, message.content, sendResponse, responseTakenCareOf);
+				}
+				else {
+					// This is ok, it happens when the background is broadcasting a message that is not listened
+					// yet on the foreground 
+					console.warn(`Could not find relayer with name '${message.relayerName}'`);
+				}
+				
+			}
+		});
+	}
+}
+
+class MessageRelayer implements IMessageRelayer {
+	private name: string;
+	private listeners: Record<string, OnMessageCallback[]> = {};
+	private browserService: any;
+	private runningInBackground: boolean;
+
+	constructor(name: string, runningInBackground: boolean, browserService: any) {
+		this.name = name;
+		this.runningInBackground = runningInBackground;
+		this.browserService = browserService;
+		// this.registerMessageListener();
+	}
+
+	onMessage(messageName: string, callback: OnMessageCallback): IMessageRelayer {
+		if(!this.listeners[messageName]) {
+			this.listeners[messageName] = [];
+		}
+		else if(this.listeners[messageName].indexOf(callback) >= 0) {
+			return;
+		}
+
+		this.listeners[messageName].push(callback);
+		return this;
+	}
+
+	dispatchToListeners(messageName: string, messageContent: any, sendResponseCallback: SendResponseCallback, responseTakenCareOf: boolean) {
+		if(!this.listeners[messageName]) {
+			return false;
+		}
+		
+		let willAnswer: boolean;
+
+		this.listeners[messageName].forEach((callback) => {
+			willAnswer = callback(messageContent, (response: any) => {
+				if(!responseTakenCareOf && sendResponseCallback) {
+					responseTakenCareOf = true;
+					sendResponseCallback(response);
+				}
+			}) ? true : willAnswer;
+		});
+
+		return willAnswer;
+	}
+
+	sendMessage(messageName: string, messageContent: any, sendResponseCallback: SendResponseCallback) {
+		let responseTakenCareOf = false;
+
+		// send the message to other receivers than the background
+		this.browserService.runtime.sendMessage({
+			fromBackground: this.runningInBackground,
+			relayerName: this.name,
+			name: messageName,
+			content: messageContent,
+		}, (response: any) => {
+			// check if it was already answered by a background listener
+			if(!responseTakenCareOf && sendResponseCallback) {
+				responseTakenCareOf = true;
+				sendResponseCallback(response);
+			}
+		});
+
+		if(this.runningInBackground) {
+			// messages sent from foreground are always treated on the runtime listener
+			this.dispatchToListeners(messageName, messageContent, sendResponseCallback, responseTakenCareOf);
+		}
 	}
 }
 
