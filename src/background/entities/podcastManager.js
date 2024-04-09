00afa14c913558c8e47e8fb1cc832a55f55574f0
Guilherme Dellagustin <dellagustin@gmail.com>
2024-04-09 22:50:22 +0200

    Local Storage Refactoring: Removed unused code from Podcast Manager

--- src/background/entities/podcastManager.js
+++ src/background/entities/podcastManager.js
@@ -14,7 +14,6 @@ function PodcastManager() {
 	var notificationIdLoading = 0;
 
 	this.setEpisodeProgress = setEpisodeProgress;
-	this.getEpisodesInProgress = getEpisodesInProgress;
 	this.getEpisodeIds = getEpisodeIds;
 	this.reset = reset;
 	this.getOpml = getOpml;
@@ -40,30 +39,6 @@ function PodcastManager() {
 				notificationIdLoading = 0;
 			}
 		}
-	}
-
-	/**
-	 * @returns {Promise<[]>}
-	 */
-	function getEpisodesInProgress() {
-		const podcastDataService = getPodcastDataService();
-		const podcastStorageService = getPodcastStorageService();
-
-		return podcastStorageService.getAllEpisodesUserData().then(function(allEpisodesUserData) {
-			return allEpisodesUserData.map(function(item) {
-				const podcast = instance.getPodcast(item.episodeSelector.podcastUrl); 
-				const episode = podcast.episodes.find(function(episode) { 
-					return item.episodeSelector.matchesId(podcastDataService.episodeId(episode, podcast.url));
-				});
-
-				// return
-				return {
-					podcast: podcast,
-					episode: episode,
-					episodeUserData: item.episodeUserData
-				}
-			}).filter(function(episodeInProgress) {return episodeInProgress.episode});
-		});
 	}
 
 	this.getPodcast = function(url) {
@@ -192,134 +167,6 @@ function PodcastManager() {
 		}
 
 		return podcast;
-	}
-
-	/**
-	 * 
-	 * @param {EpisodeId} currentEpisodeId 
-	 * @param {Number} delta 
-	 * @param {Function} callback 
-	 */
-	function getEpisodeFromPodcast(currentEpisodeId, delta, callback) {
-		const podcast = instance.getPodcast(currentEpisodeId.values.podcastUrl);
-
-		const indexOfCurrentEpisode = podcast.episodes.findIndex(function(episode) {
-			return getPodcastDataService().episodeMatchesId(episode, podcast.url, currentEpisodeId);
-		});
-
-		const indexWithDelta = indexOfCurrentEpisode + delta;
-
-		if(indexWithDelta >= 0 && indexWithDelta < podcast.episodes.length) {
-			callback(getPodcastDataService().episodeId(podcast.episodes[indexWithDelta], podcast.url));
-		}
-	}
-
-	/**
-	 * 
-	 * @param {EpisodeId} currentEpisodeId 
-	 * @param {Number} delta 
-	 * @param {Function} callback 
-	 */
-	function getEpisodeFromLastEpisodes(currentEpisodeId, delta, callback) {
-		const allEpisodes = instance.getAllEpisodes();
-
-		const indexOfCurrentEpisode = allEpisodes.findIndex(function(entry) {
-			return getPodcastDataService().episodeMatchesId(entry.episode, entry.podcast.url, currentEpisodeId);
-		});
-				
-		const indexWithDelta = indexOfCurrentEpisode + delta;
-
-		if(indexWithDelta >= 0 && indexWithDelta < allEpisodes.length) {
-			callback(getPodcastDataService().episodeId(allEpisodes[indexWithDelta].episode, allEpisodes[indexWithDelta].podcast));
-		}
-	}
-
-	/**
-	 * 
-	 * @param {EpisodeId} currentEpisodeId 
-	 * @param {Number} delta 
-	 * @param {Function} callback 
-	 */
-	function getEpisodeFromPlaylist(currentEpisodeId, delta, callback) {
-		getMessageService().for('playlist').sendMessage('get', {}, function(response) {
-			if(!response) {
-				// TODO: investigate why the messageService is calling the response
-				// callback with empty response before calling it with a proper
-				// reponse
-				console.log('podcastManager - get playlist - empty reponse');
-				return;
-			}
-
-			var indexOfCurrentEpisode = response.entries.findIndex(function(playlistEpisodeId) {
-				return getPodcastDataService().episodeIdEqualsId(currentEpisodeId, playlistEpisodeId);
-			});
-
-			var newIndex = 0;
-			
-			if(indexOfCurrentEpisode >= 0) {
-				// The playlist goes backwards.
-				// For the playlist we want the next to go
-				// "down" the list, not up.
-				const indexWithDelta = indexOfCurrentEpisode - delta;
-
-				newIndex = indexWithDelta >= 0 && indexWithDelta < response.entries.length ? indexWithDelta : 0;
-			}
-
-			callback(response.entries[newIndex]);
-		});
-	}
-
-	function getEpisode(order, currentEpisode, delta, callback) {
-		switch(order ? order : 'from_last_episodes') {
-			case 'from_podcast':
-			default:
-				getEpisodeFromPodcast(currentEpisode, delta, callback);
-				break;
-			case 'from_last_episodes':
-				getEpisodeFromLastEpisodes(currentEpisode, delta, callback);
-				break;
-			case 'from_playlist':
-				getEpisodeFromPlaylist(currentEpisode, delta, callback);
-				break;
-		}
-	}
-
-	this.getNextOrPreviousEpisode = function(isNext, order, currentEpisode, callback) {
-		getEpisode(order, currentEpisode, isNext ? -1 : 1, callback);
-	};
-
-	this.deleteAllPodcasts = function () {
-		getBrowserService().storage.sync.set({'syncPodcastList': []});
-		this.podcastList.forEach(function(item) {
-			item.deleteFromStorage();
-		});
-		this.podcastList = [];
-
-		sendPodcastListChangedMessage();
-	}
-
-	this.getAllEpisodes = function(episodesFilter) {
-		var allEpisodes = [];
-
-		this.podcastList.forEach(function(podcast, podcastIndex) {
-			podcast.episodes.forEach(function(episode) {
-				if(episodesFilter && !episodesFilter(podcast, episode))
-					return;
-
-				var episodeContainer = {
-					podcastIndex: podcastIndex,
-					podcast: podcast,
-					episode: episode,
-					pubDate: new Date(episode.pubDate)
-				};
-
-				allEpisodes.push(episodeContainer);
-			});
-		});
-
-		allEpisodes.sort(function (a, b) { return b.pubDate - a.pubDate; });
-
-		return allEpisodes;
 	}
 
 	this.getPodcastAndEpisode = function(episodeId) {
